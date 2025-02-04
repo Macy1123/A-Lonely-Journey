3b4362c21c3b84931cfd135158734f705bd96500
KingCleric <JELyons1@student.fullsail.edu>
2025-02-04 15:50:07 -0500

    Started AI work

---
+++ Source/A_Lonely_Journey/BP_BaseEnemy.h
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "BP_BaseEnemy.generated.h"
+
+UCLASS()
+class A_LONELY_JOURNEY_API ABP_BaseEnemy : public ACharacter
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ABP_BaseEnemy();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
